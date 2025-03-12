dff06359ee85698054fce760da5f584e3a99fb1c
iftekharul01 <iftekharulislam001@gmail.com>
2025-03-12 10:07:05 +0600

    color pallete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home/page/home.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const FreebiesEcom());
 }
 
+//tinyurl.com/cse414-figma
+
+class FreebiesEcom extends StatelessWidget {
+  const FreebiesEcom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Freebies E-Com",
+      home: Home(),
+    );
+  }
+}
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const primaryblue = Color(0xFF5CC936);
+  static const primaryorange = Color(0xFFFFC120);
+  static const primaryblack = Color(0xFF0C1A30);
+  static const primarywhite = Color(0xFFFFFFFF);
+}
