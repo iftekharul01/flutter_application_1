19218487de326375b4623606a9580d1244c1dfef
iftekharul01 <iftekharulislam001@gmail.com>
2025-03-12 10:43:25 +0600

    appbarSeparate

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -8,27 +8,41 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Mega shop',
-          style: TextStyle(
-            fontFamily: "DMSans",
-            fontWeight: FontWeight.w700,
-            color: FColors.oceanBlue,
-          ),
+      appBar: FAppBar(),
+    );
+  }
+}
+
+class FAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const FAppBar({
+    super.key,
+  });
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        'Mega shop',
+        style: TextStyle(
+          fontFamily: "DMSans",
+          fontWeight: FontWeight.w700,
+          color: FColors.oceanBlue,
         ),
-        centerTitle: true,
-        actions: [
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
-            child: Icon(Iconsax.notification_bing_outline),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(5, 10, 10, 10),
-            child: Icon(IonIcons.cart),
-          ),
-        ],
       ),
+      centerTitle: true,
+      actions: [
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(Iconsax.notification_bing_outline),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+          child: Icon(IonIcons.cart),
+        ),
+      ],
     );
   }
 }
